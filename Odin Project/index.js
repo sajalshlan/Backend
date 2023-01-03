0316db250d45f2ac50e85291eb9b3081db1560b0
Sajal Sharma <sajalsharma2408@gmail.com>
2023-01-03 22:41:01 +0530

    module2 completed, now net ninja

--- Odin Project/index.js
+++ Odin Project/index.js
@@ -7,6 +7,9 @@ http
   })
   .listen(8080);
 
+//Now, my computer works as a server!
+//If anyone tries to access my computer on port 8080, they will get a "hello world!" message in return!
+
 const options = {
   hostname: "google.com",
   port: 8080,
